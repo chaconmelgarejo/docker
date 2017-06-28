6772d4e8fd37d52b34f07e1b9df2666f8548473c
Alejandro JCM <chaconmelgarejo@gmail.com>
2017-06-28 11:07:17 -0400

    Create install.sh
    for ubuntu 16.04 and 64bits.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Setting up repository"
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+echo "update repository"
+sudo apt-get update
+echo "install dc"
+sudo apt-get install docker-ce
